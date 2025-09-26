cd3f024dd2a1f06f051acc860bac38b8f3967a79
Ángel Nereira <angelnereira15@gmail.com>
2025-09-26 16:30:19 +0000

    que esta barra de busqueda sea flex y se adapte al contenido alrededor a

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -224,7 +224,7 @@ function DashboardLayoutContent({
                 </div>
               </SheetContent>
             </Sheet>
-            <div className="w-full flex-1">
+            <div className="flex-1">
               <form>
                 <Tooltip>
                   <TooltipTrigger className="w-full text-left">
@@ -233,7 +233,7 @@ function DashboardLayoutContent({
                         <Input
                             type="search"
                             placeholder="Buscar documentos, clientes, RUC..."
-                            className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+                            className="w-full appearance-none bg-background pl-8 shadow-none"
                         />
                     </div>
                   </TooltipTrigger>
