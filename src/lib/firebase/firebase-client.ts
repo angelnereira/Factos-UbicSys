569c8179379d90ca3380a331394ac10041dc89b9
Ángel Nereira <angelnereira15@gmail.com>
2025-09-29 20:18:50 +0000

    Si ya está usando npm y un paquete de módulos como webpack o Rollup , pu

--- src/lib/firebase/firebase-client.ts
+++ src/lib/firebase/firebase-client.ts
@@ -9,16 +9,16 @@ import { getFirestore, type Firestore } from 'firebase/firestore';
 // This is a public configuration and it's safe to be exposed.
 // Security is enforced by Firebase Security Rules on the backend.
 const firebaseConfig = {
-  apiKey: "AIzaSyBx11TjLWQvQtuFbIOL753IPHRlq9K1gLI",
+  apiKey: "AIzaSyATnNpGkVVDXiCVJX3sBI388h5koK9iylU",
   authDomain: "studio-7875732711-2b800.firebaseapp.com",
   projectId: "studio-7875732711-2b800",
-  storageBucket: "studio-7875732711-2b800.appspot.com",
-  messagingSenderId: "7875732711",
-  appId: "1:7875732711:web:dd880b435abb508215c024"
+  storageBucket: "studio-7875732711-2b800.firebasestorage.app",
+  messagingSenderId: "133672149206",
+  appId: "1:133672149206:web:8c1729a1fc322879f36977"
 };
 
-let app: FirebaseApp | null = null;
-let auth: Auth | null = null;
+let app: FirebaseApp;
+let auth: Auth;
 let db: Firestore | null = null;
 
 // This check ensures Firebase is only initialized on the client-side.
@@ -31,15 +31,14 @@ if (typeof window !== 'undefined') {
         app = getApp();
     }
 
-    if (app) {
-        auth = getAuth(app);
-        try {
-            db = getFirestore(app);
-        } catch (e) {
-            console.warn("Firestore is not available. Please enable it in your Firebase project.");
-            db = null;
-        }
+    auth = getAuth(app);
+    try {
+        db = getFirestore(app);
+    } catch (e) {
+        console.warn("Firestore is not available. Please enable it in your Firebase project.");
+        db = null;
     }
 }
 
+// @ts-ignore
 export { app, auth, db };
